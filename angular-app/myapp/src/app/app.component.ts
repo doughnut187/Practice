1db0c03185eaab94db7ac0ebb98c4868bd134f96
doughnut187 <tjoyce@uoregon.edu>
2022-08-15 12:50:13 -0700

    angular todo list created

---
+++ angular-app/myapp/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  //state
+  title = 'myapp';
+  todos: string[] = [];
+  todoText = '';
+
+  //lifecycle
+  ngOnInit() {
+    const existingTodos = localStorage.getItem('todos');
+    this.todos = JSON.parse(existingTodos as string) || [];
+  }
+
+  //events
+  addTodo() {
+    this.todos.push(this.todoText);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+    this.todoText = '';
+  }
+}
